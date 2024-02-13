ac9fc53a54ea27581c32b5588b6ef8329fc0d408
romw314 <106016361+romw314@users.noreply.github.com>
2024-02-13 15:06:55 +0100

    cmake

--- best-copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-export NO_BEST_COPY=1
-unset SSH_COPY
-unset DEFAULT_COPY
-sudo /root/mount.sh
-MY_RH=/mnt/gigabyte/romanko/.local
-rm -rf $MY_RH/bin/movement-zd $MY_RH/bin/movement-zd-stable
-exec ./copygigabyte.sh
--- src/main/cpps.h
+++ src/main/cpps.h
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <utility>
+#include <string>
 
 #define to_chptr(x) (std::to_string(x).c_str())
 #define to_chptr_as(x,y) ((std::to_string(x) + (y)).c_str())
